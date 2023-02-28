31c82148a774e59605fe8857f32df640ad7dd1e8
Maylon-tech <minatokiroisen77@gmail.com>
2023-02-28 23:14:46 +0900

    adicionando metodo get

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,8 +1,13 @@
 const ProductsModel = require('../models/products')
 
+// Funciton assincrona no Metodo GET
 async function get(req, res) {
-    const products = await ProductsModel.find()
+    const { id } = req.params
 
+    // Se Tiver ID (true) entao retorne . senao {} vazio.
+    const obj = id ? { _id: id } : null
+    // Procura produtos no BD com metodo find()
+    const products = await ProductsModel.find(obj)  
 
     res.send(products)
 
